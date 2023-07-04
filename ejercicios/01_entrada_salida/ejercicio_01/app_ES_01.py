3fdd6aa2593ff99651c8cd4ba4b08301a4a31dba
Facundo Duque <facundoduque63@gmail.com>
2023-07-04 12:14:58 -0300

    Subi el ejercicio 1

--- ejercicios/01_entrada_salida/ejercicio_01/app_ES_01.py
+++ ejercicios/01_entrada_salida/ejercicio_01/app_ES_01.py
@@ -6,8 +6,8 @@ import customtkinter
 
 
 '''
-nombre:
-apellido:
+nombre:facundo 
+apellido:duque 
 ---
 Ejercicio: entrada_salida_01
 ---
@@ -29,8 +29,10 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
+        #Esto no anda,funciona
+        alert(title="EJ 01", message="Esto no anda,funciona")
         """agregue una linea"""
-        pass
+        
 
 
 if __name__ == "__main__":
