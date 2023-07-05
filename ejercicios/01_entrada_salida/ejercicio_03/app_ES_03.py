b993dcd54b633ac2df78ebc7b5829b3590330f61
Facundo Duque <facundoduque63@gmail.com>
2023-07-05 11:49:54 -0300

    subir ejercicios de hoy

--- ejercicios/01_entrada_salida/ejercicio_03/app_ES_03.py
+++ ejercicios/01_entrada_salida/ejercicio_03/app_ES_03.py
@@ -35,7 +35,9 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre=self.txt_nombre.get()
+        alert(title="EJ 03", message=nombre)
+        
                 
     
 if __name__ == "__main__":
--- ejercicios/01_entrada_salida/ejercicio_05/app_ES_05.py
+++ ejercicios/01_entrada_salida/ejercicio_05/app_ES_05.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre:facundo
+apellido:duque
 ---
 Ejercicio: entrada_salida_05
 ---
@@ -41,7 +41,7 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        prompt()
 
 
         
